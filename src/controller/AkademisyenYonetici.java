837fef367cddea7f015a9bd551386aa792708442
Yusuf Ali Çezik <yusufalicezik34@gmail.com>
2018-05-21 22:46:08 +0300

    .....

--- src/controller/AkademisyenYonetici.java
+++ src/controller/AkademisyenYonetici.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+﻿
 package controller;
 
 import java.util.List;
@@ -16,10 +12,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-/**
- *
- * @author HackerRıdvan
- */
+
 public class AkademisyenYonetici {
      private JTable akademisyenTablo;
     private final static String SORGU_KALIP = "from Ogretimuyesi m";
--- src/view/NewJFrame.java
+++ src/view/NewJFrame.java
@@ -1,4 +1,4 @@
-    /*
+﻿    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,12 +9,6 @@ import controller.AkademisyenYonetici;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-
-
-/**
- *
- * @author HackerRıdvan
- */
 public class NewJFrame extends javax.swing.JFrame {
 
     private AkademisyenYonetici ay;
